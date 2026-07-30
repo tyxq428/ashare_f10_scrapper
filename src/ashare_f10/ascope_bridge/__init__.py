4d46d46304274cc56e0913bf6a2205de505a3a29
tyxq428 <tyxq428@gmail.com>
2026-07-30 03:04:18 -0700

    Add A-SCOPE request bridge package

---
+++ src/ashare_f10/ascope_bridge/__init__.py
@@ -0,0 +1,15 @@
+from ashare_f10.ascope_bridge.request_package import (
+    RequestManifest,
+    RequestPackageError,
+    RequestRow,
+    ResolvedRequest,
+    resolve_request_package,
+)
+
+__all__ = [
+    "RequestManifest",
+    "RequestPackageError",
+    "RequestRow",
+    "ResolvedRequest",
+    "resolve_request_package",
+]
